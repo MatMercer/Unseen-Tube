4017edb6f47d3d7a1205d642a1ecf952046408c4
MatMercer <mateusmercer@gmail.com>
2017-07-17 15:01:40 -0300

    Add Published before option

--- src/app/unseen-tube/unseen-tube.component.ts
+++ src/app/unseen-tube/unseen-tube.component.ts
@@ -6,11 +6,17 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./unseen-tube.component.css']
 })
 export class UnseenTubeComponent implements OnInit {
-  searchQuery = 'webdriver torso';
-  isSearching = false;
-  maxViews = 500;
+  searchQuery: string;
+  isSearching: boolean;
+  maxViews: number;
+  publishedBefore: number;
 
   constructor() {
+    /* Variables setup */
+    this.searchQuery = 'webdriver torso';
+    this.isSearching = false;
+    this.maxViews = 500;
+    this.publishedBefore = new Date().getFullYear();
   }
 
   ngOnInit() {
