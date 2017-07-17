a52613e76be9fcc57a1a751c17b6b03aa2dfb956
MatMercer <mateusmercer@gmail.com>
2017-07-17 13:34:26 -0300

    Remove maxViews range

--- src/app/unseen-tube/unseen-tube.component.ts
+++ src/app/unseen-tube/unseen-tube.component.ts
@@ -8,20 +8,7 @@ import {Component, OnInit} from '@angular/core';
 export class UnseenTubeComponent implements OnInit {
   searchQuery = 'webdriver torso';
   isSearching = false;
-  maxViewsRange = [100, 500];
-
-  /* maxViewsRange slider config */
-  maxViewsRangeInputConfig: any = {
-    behaviour: 'drag',
-    connect: true,
-    start: [10, 500],
-    keyboard: true,  // same as [keyboard]="true"
-    step: 15,
-    range: {
-      min: 0,
-      max: 1000
-    }
-  };
+  maxViews = 500;
 
   constructor() {
   }
