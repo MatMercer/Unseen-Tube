feabc95e208f2b80c85150e288a640dd82c8ee44
MatMercer <mateusmercer@gmail.com>
2017-07-20 14:52:09 -0300

    Setup nextPageToken

--- src/app/unseen-tube/unseen-tube.service.ts
+++ src/app/unseen-tube/unseen-tube.service.ts
@@ -2,6 +2,13 @@ import { Injectable } from '@angular/core';
 import {Http, URLSearchParams} from '@angular/http';
 
 export class UnseenTubeQuery {
+  get nextPageToken(): string {
+    return this._nextPageToken;
+  }
+
+  set nextPageToken(value: string) {
+    this._nextPageToken = value;
+  }
   get publishedBefore(): number {
     return this._publishedBefore;
   }
@@ -17,6 +24,7 @@ export class UnseenTubeQuery {
   private _searchQuery: string;
   private _maxViews: number;
   private _publishedBefore: number;
+  private _nextPageToken: string;
 
   constructor(searchQuery: string, maxViews?: number, publishedBefore?: number) {
     this._searchQuery = searchQuery;
@@ -35,35 +43,52 @@ export class UnseenTubeQuery {
 
 @Injectable()
 export class UnseenTubeService {
+  /* API setup */
   private API_KEY = 'AIzaSyACIZPKUPvLwV9uhajLzI-zmBjlpBzbHYg';
   private API_URL = 'https://www.googleapis.com/youtube/v3/search';
 
-  constructor(private http: Http) {
-  }
+  /* The current query */
+  private currentQuery: UnseenTubeQuery;
 
-  performSearch(query: UnseenTubeQuery) {
+  constructor(private http: Http) {}
+
+  /**
+   * Performs a Youtube search.
+   * @param newQuery
+   */
+  performSearch(newQuery: UnseenTubeQuery) {
+
+    /*
+     Updates the current query for later use
+     if the user wants to use the next page
+     */
+    this.currentQuery = newQuery;
+
+    /*
+      Creates the API parameters
+     */
     const params: URLSearchParams = new URLSearchParams();
-
     params.set('part', 'id');
-    params.set('q', query.searchQuery);
+    params.set('q', newQuery.searchQuery);
     params.set('type', 'video');
-    params.set('publishedBefore', encodeURI(query.publishedBefore + '-01-01T00:00:00Z'));
+    params.set('publishedBefore', encodeURI(newQuery.publishedBefore + '-01-01T00:00:00Z'));
     params.set('maxResults', '50');
     params.set('key', this.API_KEY);
 
-    const results = this.http
+    /* Makes the API request with the parameters */
+    this.http
       .get(this.API_URL, {
         search: params
       }).subscribe(
         (response) => this.onSearchSuccess(response.json()),
         (error) => this.onSearchError(error.json())
       );
-
-    console.log(results);
   }
 
-  private onSearchSuccess(response: JSON) {
+  private onSearchSuccess(response) {
     console.log(response);
+    /* Setup the next page token for later use */
+    this.currentQuery.nextPageToken = response.nextPageToken;
   }
 
   private onSearchError(error: JSON) {
